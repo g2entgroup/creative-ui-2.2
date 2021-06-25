07dbe432ece179d6b8e59c765fe75a346d66774f
Rashmi-278 <rashmiabbigeri@gmail.com>
2021-06-26 00:05:38 +0530

    discover page

---
+++ src/pages/discover.tsx
@@ -0,0 +1,16 @@
+const brandcards = 8;
+import { SimpleGrid, Text } from "@chakra-ui/react"
+import BrandDiscovery from '../components/common/Cards/branddiscovery'
+export default function Discover() {
+    return(
+        <>
+        <Text as="h1" fontSize="4xl" fontStyle="bold" margin="10">Discover and Explore</Text>
+        <SimpleGrid columns={{sm: 1, md: 4}}>
+            <BrandDiscovery imagelink= "https://picsum.photos/200/300?random=1" name="@creator" bio="awesome creative NFT"/>
+            <BrandDiscovery imagelink= "https://picsum.photos/200/300?random=2" name="@creator" bio="awesome creative NFT"/>
+            <BrandDiscovery imagelink= "https://picsum.photos/200/300?random=3" name="@creator" bio="awesome creative NFT"/>
+            <BrandDiscovery imagelink= "https://picsum.photos/200/300?random=4" name="@creator" bio="awesome creative NFT"/>
+        </SimpleGrid>
+        </>
+    )
+}
