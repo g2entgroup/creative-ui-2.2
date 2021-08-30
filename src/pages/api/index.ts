f1812747fd6324a4381b4926d9cda665f01e6eb4
G2 <bracy.gawain2014@gmail.com>
2021-08-30 14:55:13 -0400

    createPayment API
    Made API endpoint that creates a payment

---
+++ src/pages/api/index.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
+
+const { CIRCLE_API_KEY } = process.env;
+
+export default async function createPayment (req: NextApiRequest, res: NextApiResponse) {
+  await NextCors(req, res, {
+    origin: "https://api-sandbox.circle.com/v1/payments",
+    methods: ["GET", "POST"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['application/json', `Bearer ${CIRCLE_API_KEY}` ],
+    optionsSuccessStatus: 200,
+  });
+
+  try {
+    res.json(createPayment);
+  } catch (e) {
+    res.status(400).json({ error: (e as Error).message });
+  }
+};
