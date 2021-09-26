df65ae210db41e1451397c29ac5d7764518d872b
G2 <bracy.gawain2014@gmail.com>
2021-09-26 01:51:49 -0400

    Updated mint()'s
    * Updated LazyMInt to Rarible button
    * Added OpenSea Mint button
    * Updated Disccover NFT page, buy()

--- src/components/common/Cards/branddiscovery.tsx
+++ src/components/common/Cards/branddiscovery.tsx
@@ -124,46 +124,27 @@ type WindowInstanceWithEthereum = Window & typeof globalThis & { ethereum?: prov
             <Badge
               px={2}
               py={1}
-              bg={useColorModeValue('gray.50', 'gray.800')}
+              bg={useColorModeValue('gray.50', 'gray.50')}
               fontWeight={'400'}>
               #art
             </Badge>
             <Badge
               px={2}
               py={1}
-              bg={useColorModeValue('gray.50', 'gray.800')}
+              bg={useColorModeValue('gray.50', 'gray.50')}
               fontWeight={'400'}>
               #photography
             </Badge>
             <Badge
               px={2}
               py={1}
-              bg={useColorModeValue('gray.50', 'gray.800')}
+              bg={useColorModeValue('gray.50', 'gray.50')}
               fontWeight={'400'}>
               #music
             </Badge>
           </Stack>
-          <Stack mt={8} direction={'row'} spacing={4}>
-            <Button
-              flex={1}
-              fontSize={'md'}
-              bg={'blue.400'}
-              color={'white'}
-              boxShadow={
-                '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
-              }
-              _hover={{
-                bg: 'blue.500',
-              }}
-              _focus={{
-                bg: 'blue.500',
-              }}
-              onClick={submitHandler}
-              >
-              Mint to Rarible
-            </Button>
-          </Stack>
-          <Stack mt={4} mb={4} spacing={1}>
+         
+          <Stack m={4} mb={4} spacing={1}>
               <Buy />
           </Stack>
         </Box>
