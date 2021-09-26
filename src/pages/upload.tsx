59e602436c4ebaa38178ee4567d557679f25f18c
G2 <bracy.gawain2014@gmail.com>
2021-09-25 21:36:26 -0400

    updated attributes folder

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -30,8 +30,8 @@ import { providers } from 'ethers'
 import { FaUser } from "react-icons/fa";
 import { useForm } from "react-hook-form";
 import { TextileInstance } from "../services/textile/textile";
-import AddAttributes from "../components/Attributes/AddAttributes";
-import AttributesList from "../components/Attributes/AttributesList";
+import AddAttributes from "../components/attributes/AddAttributes";
+import AttributesList from "../components/attributes/AttributesList";
 type WindowInstanceWithEthereum = Window & typeof globalThis & { ethereum?: providers.ExternalProvider };
   class StrongType<Definition, Type> {
     // @ts-ignore
