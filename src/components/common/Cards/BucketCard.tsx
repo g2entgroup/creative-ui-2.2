5495bf6a445b09303deecbb2145f3084e45b90cc
Gawain Bracy II <bracy.gawain2014@gmail.com>
2021-10-27 23:19:05 -0400

    Celo Mint
    Added Celo Mint button

--- src/components/common/Cards/BucketCard.tsx
+++ src/components/common/Cards/BucketCard.tsx
@@ -241,6 +241,26 @@ type WindowInstanceWithEthereum = Window & typeof globalThis & { ethereum?: prov
             </Button>
             {/* <Sell /> */}
           </HStack>
+          <HStack m={4} direction={'row'} spacing={4}>
+          <Button
+              flex={1}
+              fontSize={'smaller'}
+              bg={'blue.400'}
+              color={'white'}
+              boxShadow={
+                '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+              }
+              _hover={{
+                bg: 'blue.500',
+              }}
+              _focus={{
+                bg: 'blue.500',
+              }}
+              onClick={submitHandler}
+              >
+              Celo Mint
+            </Button>
+          </HStack>
         </Box>
     </>
     );
