e37ab45c72201ef473ee73ffd0aff863726c0cb3
G2 <bracy.gawain2014@gmail.com>
2021-11-01 01:23:53 -0400

    Jest/babel fix
    - updated config files to use babel-jest and @babel/preset-typescript
    - updated types/node

--- jest.config.ts
+++ jest.config.ts
@@ -93,7 +93,7 @@ export default {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+   preset: 'ts-jest',
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -172,10 +172,10 @@ export default {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+   transform: {'^.+\\.tsx?$': 'babel-jest', }
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
+  // transformIg}norePatterns: [
   //   "\\\\node_modules\\\\",
   //   "\\.pnp\\.[^\\\\]+$"
   // ],
