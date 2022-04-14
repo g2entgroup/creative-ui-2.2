93f29b01f93d024b18ff51746a91fd2043f9540f
Gawain Bracy II <gawainbracyii@gawains-mbp.lan>
2022-04-14 11:15:45 -0400

    Balance
    Update to include wallet balance

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,16 +1,47 @@
-import { Text } from '@chakra-ui/react'
-import { useEtherBalance, useEthers} from '@usedapp/core'
-import { utils } from 'ethers'
+import { useState, useEffect } from "react";
+import { Text } from '@chakra-ui/react';
+import { useEtherBalance, useEthers} from '@usedapp/core';
+import useSWR from "swr";
+import { fetcher } from "../utils/myFetcher";
+import { utils } from 'ethers';
+import { formatEther } from "@ethersproject/units";
+import { useFetchPairPrice } from '../services/coingecko/useServices'
+
+
 
 /**
  * Component
  */
 function Balance(): JSX.Element {
-
-  const { account, chainId } = useEthers()
+  const [fiatValue, setFiatValue] = useState(0);
+  const { account, chainId, library } = useEthers()
   const etherBalance = useEtherBalance(account)
-  const finalBalance = etherBalance ? utils.formatEther(etherBalance) : ''
-  return <Text>{finalBalance.slice(0,4)} ETH </Text>
+  const finalBalance = etherBalance ? formatEther(etherBalance) : ''
+  const addBalance = fiatValue * parseFloat(finalBalance);
+  let chain = chainId;
+  let currency;
+  
+
+  const {
+    data: priceData,
+    loading: priceLoading,
+    error: priceError,
+  } = useFetchPairPrice("matic-network", "usd");
+
+  const { data: balance } = useSWR(["getBalance", account, "latest"], {
+    fetcher: fetcher(library),
+  });
+
+  useEffect(() => {
+    priceData; 
+    setFiatValue(priceData?.["matic-network"]?.usd);
+  })
+  
+  return (
+    account && chain === 80001 || 137 ?
+    (currency = <Text>{finalBalance.slice(0,4)} MATIC ${(addBalance).toFixed(2)}</Text>) :
+    (currency = <Text>{finalBalance.slice(0,4)} ETH ${(addBalance).toFixed(2)}</Text>)
+  )
 }
 
-export default Balance
+export default Balance;
