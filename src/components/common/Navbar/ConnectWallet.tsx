0dd9e132b0768268cefe3d4cc2abba4f8ad1d810
Gawain Bracy II <gawainbracyii@gawains-mbp.lan>
2022-05-11 10:38:45 -0400

    Clean Up

--- src/components/common/Navbar/ConnectWallet.tsx
+++ src/components/common/Navbar/ConnectWallet.tsx
@@ -1,6 +1,3 @@
-import {
-  Box
-} from '@chakra-ui/react';
 import React from 'react';
 import { Web3ModalButton } from './Account/Web3ModalButton';
 
@@ -8,12 +5,7 @@ function ConnectWallet(): JSX.Element {
 
   return (
     <>
-      <Box
-        order={[null, null, null, null]}
-        textAlign={[null, null, null, null]}
-      >
         <Web3ModalButton />
-      </Box>
     </>
   )
 }
