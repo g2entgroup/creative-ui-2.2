7b85200fbe5d55abfb93676705116851b4df6a35
Gawain Bracy II <gawainbracyii@gawains-mbp.lan>
2022-06-01 11:32:37 -0400

    remove ens
    refactoring handling onf ENS

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -18,12 +18,11 @@ import {
   chakra,
 } from "@chakra-ui/react";
 import CreativeCard from "../../components/common/Cards/CreativeCard";
-import { useEthers, shortenAddress, useLookupAddress } from "@usedapp/core";
+import { useEthers, shortenAddress } from "@usedapp/core";
 import { FaTwitter, FaInstagram, FaStar } from "react-icons/fa";
 import { CheckIcon, CloseIcon, EditIcon } from "@chakra-ui/icons";
 
 const Profile = () => {
-  const ens = useLookupAddress();
   const { account } = useEthers();
   const [twitter, setTwitter] = React.useState("@username");
   const [instagram, setInstagram] = React.useState("@username");
@@ -169,7 +168,7 @@ const Profile = () => {
             <Box marginBottom={[10, 10, 0, 0]}>
               <Box display={["flex", "flex", "flex", "flex"]}>
                 <chakra.h2 color="white" fontSize="md" fontWeight="medium">
-                  {ens ?? shortenAddress(account)}
+                  {shortenAddress(account)}
                 </chakra.h2>
               </Box>
               <Box display="flex" alignContent={"left"} padding={2}>
