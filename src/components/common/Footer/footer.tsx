f8ffc4f941533a0e913c809620065d8e5e855932
Gawain Bracy II <gawainbracyii@gawains-mbp.lan>
2022-06-03 15:07:31 -0400

    UI updates
    minor UI updates

--- src/components/common/Footer/footer.tsx
+++ src/components/common/Footer/footer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import {
   Grid,
   Flex,
@@ -8,19 +8,20 @@ import {
   Input,
   Link,
   useColorModeValue,
-} from "@chakra-ui/react";
-import { AiOutlineMail } from "react-icons/ai";
-import { FiTwitter, FiInstagram } from "react-icons/fi";
-import { SiDiscord } from "react-icons/si";
-import Image from "next/image";
+  chakra,
+} from '@chakra-ui/react'
+import { AiOutlineMail } from 'react-icons/ai'
+import { FiTwitter, FiInstagram } from 'react-icons/fi'
+import { SiDiscord } from 'react-icons/si'
+import Image from 'next/image'
 
 const myLoader = ({ src, width }) => {
-  return `https://res.cloudinary.com/${src}?w=${width}&q=${75}`;
-};
+  return `https://res.cloudinary.com/${src}?w=${width}&q=${75}`
+}
 
 const Footer = () => {
   return (
-    <Box flexDir={["column", "column", "column", "row"]} bgColor="#161d2f">
+    <Box flexDir={['column', 'column', 'column', 'row']} bgColor="#161d2f">
       {/* logo  */}
       <Box textAlign="center" mb="3rem" padding={10}>
         <Image
@@ -30,7 +31,7 @@ const Footer = () => {
           width={90}
           height={80}
         />
-        <Heading fontSize="2rem" color={"white"}>
+        <Heading fontSize="2rem" color={'white'}>
           CREATIVE
         </Heading>
       </Box>
@@ -38,10 +39,10 @@ const Footer = () => {
       <Box
         width="100%"
         display="flex"
-        flexDir={["column", "column", "row", "row"]}
-        flexWrap={["nowrap", "nowrap", "wrap", "wrap"]}
+        flexDir={['column', 'column', 'row', 'row']}
+        flexWrap={['nowrap', 'nowrap', 'wrap', 'wrap']}
       >
-        <Box maxWidth={["100%", "50%", "50%", "25%"]} padding={5}>
+        <Box maxWidth={['100%', '50%', '50%', '25%']} padding={5}>
           <Box as="h3" fontWeight="500" fontSize="xl" color="#e50168">
             Creative Platform
           </Box>
@@ -52,13 +53,13 @@ const Footer = () => {
             w="50px"
             background="linear-gradient(to right, rgba(22, 29, 47, 0),#e50168,rgba(22,29,47,0));"
           />
-          <Text color={"white"}>
+          <Text color={'white'}>
             A creative blockchain platform for independent creators, fans, and
             brands to exchange services directly and earn incentives through
             DeFi.
           </Text>
         </Box>
-        <Box maxWidth={["100%", "550%", "50%", "25%"]} padding={5}>
+        <Box maxWidth={['100%', '550%', '50%', '25%']} padding={5}>
           <Box as="h3" fontWeight="500" fontSize="xl" color="#e50168">
             Join Our Community DAO
           </Box>
@@ -69,31 +70,30 @@ const Footer = () => {
             w="50px"
             background="linear-gradient(to right, rgba(22, 29, 47, 0),#e50168,rgba(22,29,47,0));"
           />
-          <Text textOverflow={"wrap"} color={"white"}>
+          <Text textOverflow={'wrap'} color={'white'}>
             Have a seat at the table and join our governance DAO, where our
             creative community helps decide the future of the Creative platform.
           </Text>
-          <Box
-            as="button"
-            bgColor="#e50168"
-            textAlign="center"
-            w="12rem"
-            p="0.5rem"
-            borderRadius="2rem"
-            mt="1rem"
-            cursor="pointer"
-            maxW={"90%"}
+          <chakra.a
+            w="full"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            px={{ base: 8, md: 10 }}
+            py={{ base: 3, md: 4 }}
+            my={4}
+            border="solid 1px transparent"
+            fontSize={{ base: 'md', md: 'lg' }}
+            rounded="md"
+            color={'white'}
+            bg="brand.400"
+            _hover={{ bg: 'pink.800' }}
+            href="https://app.daohaus.club/dao/0x89/0xc48996a569911fd6eba1b97b6419731eed32041e"
           >
-            <Link
-              href="https://app.daohaus.club/dao/0x89/0xc48996a569911fd6eba1b97b6419731eed32041e/"
-              isExternal
-              target="_blank"
-            >
-              Creative Organization
-            </Link>
-          </Box>
+            Creative Organization DAO
+          </chakra.a>
         </Box>
-        <Box width={["100%", "50%", "50%", "25%"]} padding={5}>
+        <Box width={['100%', '50%', '50%', '25%']} padding={5}>
           <Box as="h3" fontWeight="500" fontSize="xl" color="#e50168">
             Subscribe
           </Box>
@@ -104,7 +104,7 @@ const Footer = () => {
             w="50px"
             background="linear-gradient(to right, rgba(22, 29, 47, 0),#e50168,rgba(22,29,47,0));"
           />
-          <Text color={"white"}>
+          <Text color={'white'}>
             Subscribe to our newsletter and get latest updates and offers.
           </Text>
 
@@ -121,7 +121,7 @@ const Footer = () => {
           />
           <Box
             as="button"
-            bgColor="#e50168"
+            bgColor="brand.400"
             textAlign="center"
             w="9rem"
             p="0.5rem"
@@ -131,7 +131,7 @@ const Footer = () => {
             Sign Me Up
           </Box>
         </Box>
-        <Box width={["100%", "50%", "50%", "25%"]} padding={4}>
+        <Box width={['100%', '50%', '50%', '25%']} padding={4}>
           <Box as="h3" fontWeight="500" fontSize="xl" color="#e50168">
             Contact Us
           </Box>
@@ -158,7 +158,7 @@ const Footer = () => {
               <Box>
                 <Link
                   href="https://discord.gg/8B4p7ztWTp"
-                  color={"white"}
+                  color={'white'}
                   isExternal
                 >
                   Discord
@@ -172,8 +172,8 @@ const Footer = () => {
             flexDir="row"
             flexWrap="wrap"
             alignItems="center"
-            maxWidth={"80%"}
-            overflowY={"hidden"}
+            maxWidth={'80%'}
+            overflowY={'hidden'}
           >
             <Box
               mr="1rem"
@@ -185,11 +185,11 @@ const Footer = () => {
             >
               <AiOutlineMail />
             </Box>
-            <Box overflow="wrap" textOverflow={"wrap"} padding={4}>
+            <Box overflow="wrap" textOverflow={'wrap'} padding={4}>
               <Box>
                 <Link
-                  color={"white"}
-                  textOverflow={"wrap"}
+                  color={'white'}
+                  textOverflow={'wrap'}
                   href="mailto:creatives@creativeplatform.xyz"
                 >
                   Email Us
@@ -217,7 +217,7 @@ const Footer = () => {
           */}
 
           <Flex mt="1rem">
-            <Box color={"white"}>Follow us:</Box>
+            <Box color={'white'}>Follow us:</Box>
             <Box
               mx="1rem"
               w="1.8rem"
@@ -226,7 +226,7 @@ const Footer = () => {
               py="0.25rem"
               borderRadius="0.4rem"
               cursor="pointer"
-              color={"white"}
+              color={'white'}
             >
               <Link
                 href="https://www.twitter.com/creativecrtv"
@@ -244,7 +244,7 @@ const Footer = () => {
               py="0.25rem"
               borderRadius="0.4rem"
               cursor="pointer"
-              color={"white"}
+              color={'white'}
             >
               <Link
                 href="https://www.instagram.com/creativecrtv"
@@ -270,10 +270,10 @@ const Footer = () => {
         />
         {/* Copyright */}
         <Box w="100%" textAlign="center">
-          <Text color={"white"}>
-            Copyright © 2021{" "}
+          <Text color={'white'}>
+            Copyright © 2021{' '}
             <Link
-              color={"white"}
+              color={'white'}
               href="https://app.daohaus.club/dao/0x89/0xc48996a569911fd6eba1b97b6419731eed32041e/"
               isExternal
             >
@@ -284,7 +284,7 @@ const Footer = () => {
         </Box>
       </Box>
     </Box>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
