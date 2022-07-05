29f03c0e21f4811973815176e0c89cadebbc905d
Gawain Bracy II <gawainbracyii@gawains-mbp.lan>
2022-07-05 15:41:38 -0400

    font sizing

--- src/styles/fontFace.ts
+++ src/styles/fontFace.ts
@@ -3,6 +3,7 @@ export default `
         font-family: 'Conthrax';
         src: url('/src/fonts/conthrax-sb-webfont.woff') format('woff'),
         url('/src/fonts/conthrax-sb-webfont.woff2') format('woff2');
+        font-display: swap;
         font-weight: normal;
         font-style: normal;
       }
