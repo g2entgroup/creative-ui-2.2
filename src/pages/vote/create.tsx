ca5a2fd127128083b2b8c1c9774215017101fb0d
Alexander Greene <greenealexander036@gmail.com>
2022-11-24 00:42:24 -0600

    fix: formatting

--- src/pages/vote/create.tsx
+++ src/pages/vote/create.tsx
@@ -1,56 +1,56 @@
-import React from "react";
-import { 
-  Box, 
-  Heading, 
-  Input, 
-  Textarea, 
-  Button, 
+import React from 'react'
+import {
+  Box,
+  Heading,
+  Input,
+  Textarea,
+  Button,
   Stack,
   InputGroup,
   InputRightElement,
-} from "@chakra-ui/react";
-import snapshot from '@snapshot-labs/snapshot.js';
-import {FaWindowClose} from 'react-icons/fa';
-import { Web3Provider } from '@ethersproject/providers';
+} from '@chakra-ui/react'
+import snapshot from '@snapshot-labs/snapshot.js'
+import { FaWindowClose } from 'react-icons/fa'
+import { Web3Provider } from '@ethersproject/providers'
 
 const hub = 'https://hub.snapshot.org'
-const client = new snapshot.Client712(hub);
+const client = new snapshot.Client712(hub)
 
 export default function Create() {
-  const [account, setAccount] = React.useState<any>("")
-  const web3 = new Web3Provider(window.ethereum);
-  const [ title, setTitle ] = React.useState<string>("");
-  const [ content, setContent ] = React.useState<string>("");
-  const [ startDate, setStartDate ] = React.useState<any>(new Date());
-  const [ startTime, setStartTime ] = React.useState<any>(new Date());
-  const [ endDate, setEndDate ] = React.useState<any>(new Date());
-  const [ endTime, setEndTime ] = React.useState<any>(new Date());
-  const [choices, setChoices] = React.useState(["yes", "no"]);
+  const [account, setAccount] = React.useState<any>('')
+  const web3 = new Web3Provider(window.ethereum)
+  const [title, setTitle] = React.useState<string>('')
+  const [content, setContent] = React.useState<string>('')
+  const [startDate, setStartDate] = React.useState<any>(new Date())
+  const [startTime, setStartTime] = React.useState<any>(new Date())
+  const [endDate, setEndDate] = React.useState<any>(new Date())
+  const [endTime, setEndTime] = React.useState<any>(new Date())
+  const [choices, setChoices] = React.useState(['yes', 'no'])
 
-  const inputRef = React.useRef();
+  const inputRef = React.useRef()
 
   const inputIsValid =
-    choices.length >= 1 && choices.every((field) => field.trim() !== "");
+    choices.length >= 1 && choices.every((field) => field.trim() !== '')
 
   function handleChange(i, event) {
-    const values = [...choices];
-    values[i] = event.target.value;
-    setChoices(values);
+    const values = [...choices]
+    values[i] = event.target.value
+    setChoices(values)
   }
 
   function handleAdd() {
-    const values = [...choices];
-    values.push("");
-    setChoices(values);
+    const values = [...choices]
+    values.push('')
+    setChoices(values)
   }
 
   function handleRemove(i) {
-    const values = [...choices];
-    values.splice(i, 1);
-    setChoices(values);
+    const values = [...choices]
+    values.splice(i, 1)
+    setChoices(values)
   }
 
-  const submit = async() => {
+  const submit = async () => {
     try {
       const provider = await snapshot.utils.getProvider('100')
       const block = await snapshot.utils.getBlockNumber(provider)
@@ -77,238 +77,205 @@ export default function Create() {
     }
   }
 
-  const getAddress = async() => {
-    const [account] = await web3.listAccounts();
-    setAccount(account);
+  const getAddress = async () => {
+    const [account] = await web3.listAccounts()
+    setAccount(account)
   }
 
-  const changeInput = (event: any, type:string) => {
-    if(type==='title'){
-      setTitle(event.target.value);
-    }
-    
-    else if(type==='content'){
-      setContent(event.target.value);
-    }
-
-    else if(type === 'start date'){
-      setStartDate(event.target.value);
-    }
-
-    else if(type === 'start time'){
+  const changeInput = (event: any, type: string) => {
+    if (type === 'title') {
+      setTitle(event.target.value)
+    } else if (type === 'content') {
+      setContent(event.target.value)
+    } else if (type === 'start date') {
+      setStartDate(event.target.value)
+    } else if (type === 'start time') {
       setStartTime(event.target.value)
-    }
-
-    else if(type === 'end time'){
+    } else if (type === 'end time') {
       setEndTime(event.target.value)
-    }
-
-    else if(type === 'end date'){
+    } else if (type === 'end date') {
       setEndDate(event.target.value)
     }
   }
 
   React.useEffect(() => {
-    getAddress();
-  },[]);
+    getAddress()
+  }, [])
 
   return (
     <Box
-      display='flex'
-      flexDirection='row'
-      flexWrap='wrap'
-      alignItems='flex-start'
-      justifyContent='center'
-      padding={10}>
-      <Box
-        padding={5}
-        width={['100%', '100%', '100%', '40%']}>
-        <Box
-          marginBottom={4}
-          cursor='pointer'>
+      display="flex"
+      flexDirection="row"
+      flexWrap="wrap"
+      alignItems="flex-start"
+      justifyContent="center"
+      padding={10}
+    >
+      <Box padding={5} width={['100%', '100%', '100%', '40%']}>
+        <Box marginBottom={4} cursor="pointer">
+          <Box padding={4} borderTopRadius={20} background={'brand.400'}>
+            <Heading size="md" color="white">
+              Title
+            </Heading>
+          </Box>
           <Box
-          padding={4}
-          borderTopRadius={20}
-          background={'brand.400'}>
-          <Heading
-            size='md'
-            color='white'>Title</Heading>
-        </Box>
-        <Box
-          background={'brand.300'}
-          padding={4}
-          borderBottomRadius={20}
-          border={'1px solid #d32f2f'}>
-          <Input 
-            color='black'
-            fontWeight={'bold'}
-            background={'white'}
-            onChange={(event) => {
-              changeInput(event, 'title')
-            }}/>
-        </Box>
-        </Box>  
-        <Box
-          marginBottom={4}
-          cursor='pointer'>
-        <Box
-          padding={4}
-          borderTopRadius={20}
-          background={'brand.400'}>
-          <Heading
-            color='white'
-            size='md'>Content</Heading>
-        </Box>
-        <Box
-          background={'brand.300'}
-          padding={4}
-          borderBottomRadius={20}
-          border={'1px solid #d32f2f'}>
-            <Textarea
-              color='black'
+            background={'brand.300'}
+            padding={4}
+            borderBottomRadius={20}
+            border={'1px solid #d32f2f'}
+          >
+            <Input
+              color="black"
               fontWeight={'bold'}
-              background={'white'} 
-              placeholder='Here is a sample placeholder'
+              background={'white'}
+              onChange={(event) => {
+                changeInput(event, 'title')
+              }}
+            />
+          </Box>
+        </Box>
+        <Box marginBottom={4} cursor="pointer">
+          <Box padding={4} borderTopRadius={20} background={'brand.400'}>
+            <Heading color="white" size="md">
+              Content
+            </Heading>
+          </Box>
+          <Box
+            background={'brand.300'}
+            padding={4}
+            borderBottomRadius={20}
+            border={'1px solid #d32f2f'}
+          >
+            <Textarea
+              color="black"
+              fontWeight={'bold'}
+              background={'white'}
+              placeholder="Here is a sample placeholder"
               onChange={(event) => {
                 changeInput(event, 'content')
-              }} />
+              }}
+            />
+          </Box>
         </Box>
-        </Box>
-        <Box
-          marginBottom={4}
-          cursor='pointer'>
-        <Box
-          padding={4}
-          borderTopRadius={20}
-          background={'brand.400'}>
-          <Heading
-            color={'white'}
-            size='md'>Choices</Heading>
-        </Box>
-        <Box
-          background={'brand.300'}
-          padding={4}
-          borderBottomRadius={20}
-          border={'1px solid #d32f2f'}>      
-          <Stack spacing={4}>
-            <form>
-            {
-                  choices.map((field, index) => {
-                  return(
-                    <InputGroup
-                      key={index}>
-                      <Input 
-                          marginBottom={5}
-                        className='choices'
-                          background='white'
-                        placeholder='Enter Choice'
-                        value={field || ""}
+        <Box marginBottom={4} cursor="pointer">
+          <Box padding={4} borderTopRadius={20} background={'brand.400'}>
+            <Heading color={'white'} size="md">
+              Choices
+            </Heading>
+          </Box>
+          <Box
+            background={'brand.300'}
+            padding={4}
+            borderBottomRadius={20}
+            border={'1px solid #d32f2f'}
+          >
+            <Stack spacing={4}>
+              <form>
+                {choices.map((field, index) => {
+                  return (
+                    <InputGroup key={index}>
+                      <Input
+                        marginBottom={5}
+                        className="choices"
+                        background="white"
+                        placeholder="Enter Choice"
+                        value={field || ''}
                         ref={inputRef}
                         onChange={(e) => handleChange(index, e)}
-                        />
-                      <InputRightElement 
-                        children={
-                        <Box
-                          onClick={() => handleRemove(index)}>
-                          <FaWindowClose />
-                        </Box>} 
                       />
-                    </InputGroup>  
+                      <InputRightElement
+                        children={
+                          <Box onClick={() => handleRemove(index)}>
+                            <FaWindowClose />
+                          </Box>
+                        }
+                      />
+                    </InputGroup>
                   )
-                }) 
-            }
-            </form>
-          </Stack>
-            
-          <Button
-            marginTop={4}
-            background={'brand.400'}
-            onClick={() => handleAdd()}>
-            <Heading
-              color='white'
-              size='sm'>
-                Add 
-            </Heading>
-          </Button>
+                })}
+              </form>
+            </Stack>
+
+            <Button
+              marginTop={4}
+              background={'brand.400'}
+              onClick={() => handleAdd()}
+            >
+              <Heading color="white" size="sm">
+                Add
+              </Heading>
+            </Button>
+          </Box>
         </Box>
-        </Box>  
       </Box>
-      <Box
-        padding={5}
-        width={['100%', '100%', '100%', '40%']}>
+      <Box padding={5} width={['100%', '100%', '100%', '40%']}>
         <Box
           padding={4}
           borderTopRadius={20}
           background={'brand.400'}
-          cursor='pointer'>
-          <Heading
-            color='white'
-            size='md'>Actions</Heading>
+          cursor="pointer"
+        >
+          <Heading color="white" size="md">
+            Actions
+          </Heading>
         </Box>
         <Box
-            background={'brand.300'}
-            padding={4}
-            borderBottomRadius={20}
-            border={'1px solid #d32f2f'}>
-            <Heading
-              color='white'
-              size='sm'>
-                Start Date
+          background={'brand.300'}
+          padding={4}
+          borderBottomRadius={20}
+          border={'1px solid #d32f2f'}
+        >
+          <Heading color="white" size="sm">
+            Start Date
+          </Heading>
+          <Input
+            type="date"
+            background={'white'}
+            onChange={(event) => {
+              changeInput(event, 'start date')
+            }}
+          />
+          <Heading marginTop={4} color="white" size="sm">
+            Start time
+          </Heading>
+          <Input
+            type="time"
+            background={'white'}
+            onChange={(event) => {
+              changeInput(event, 'start time')
+            }}
+          />
+          <Heading marginTop={4} color="white" size="sm">
+            End date
+          </Heading>
+          <Input
+            type="date"
+            background={'white'}
+            onChange={(event) => {
+              changeInput(event, 'end date')
+            }}
+          />
+          <Heading marginTop={4} color="white" size="sm">
+            End time
+          </Heading>
+          <Input
+            type="time"
+            background={'white'}
+            onChange={(event) => {
+              changeInput(event, 'end time')
+            }}
+          />
+          <Button
+            marginTop={4}
+            background={'brand.400'}
+            onClick={() => submit()}
+          >
+            <Heading color="white" size="sm">
+              Submit
             </Heading>
-            <Input 
-              type="date"
-              background={'white'}
-              onChange={(event) => {
-                changeInput(event, 'start date')
-              }}/>
-            <Heading
-              marginTop={4}
-              color='white'
-              size='sm'>
-                Start time
-            </Heading>
-            <Input 
-              type="time"
-              background={'white'}
-              onChange={(event) => {
-                changeInput(event, 'start time')
-              }}/>
-            <Heading
-              marginTop={4}
-              color='white'
-              size='sm'>
-                End date
-            </Heading>
-            <Input 
-              type="date"
-              background={'white'}
-              onChange={(event) => {
-                changeInput(event, 'end date')
-              }}/>
-            <Heading
-              marginTop={4}
-              color='white'
-              size='sm'>
-                End time
-            </Heading>
-            <Input 
-              type='time'
-              background={'white'}
-              onChange={(event) => {
-                changeInput(event, 'end time')
-              }}/>
-            <Button
-              marginTop={4}
-              background={'brand.400'}
-              onClick={() =>  submit()}>
-              <Heading
-                color='white'
-                size='sm'>
-                  Submit
-              </Heading>
-            </Button>
-        </Box>  
+          </Button>
+        </Box>
       </Box>
     </Box>
-  );
+  )
 }
